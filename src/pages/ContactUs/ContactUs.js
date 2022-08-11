cc50fd55c83acb19b189655d805daddc0dc09842
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-08-10 21:29:40 -0700

    Added a form with validation.  Working ona  custom hook

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,10 +1,58 @@
-export const ContactUs = () => {
-    return (
-        <>
+import {useState} from "react";
+
+export const ContactUs = (props) => {
+
+//  Custom hook is gonna go here.  I need to fiinish up.  I need to watch the vid that covers custom hooks.
+    let formIsValid = false
+
+    if(enteredNameIsValid) {
+        formIsValid = true
+    }
+
+
+
+
+
+    const formSubmissionHandler = event => {
+        event.preventDefault()
+        setEnteredNameIsTouched(true)
+
+        if (!enteredNameIsValid) {
+
+            return
+        }
+        setEnteredName('')
+        setEnteredNameIsTouched(false)
+
+    }
+
+
+    return (<>
             <div>
-                <p>This is where the from is gonna go to contact us!</p>
+                <p>Submit your info and we'll get back to you as soon as we can! </p>
+                <h1>Contact Us</h1>
+                <p>Est Lorem cupidatat cupidatat est irure minim consequat. Amet sint eu nisi deserunt dolor velit culpa
+                    tempor aliquip amet et amet adipisicing qui.</p>
+                <h1>Live Chat</h1>
+                <p>Est Lorem cupidatat cupidatat est irure minim consequat. Amet sint eu nisi deserunt dolor velit culpa
+                    tempor aliquip amet et amet adipisicing qui.</p>
             </div>
-            <form>Hello</form>
+
+            <form onSubmit={formSubmissionHandler}>
+                <div>
+                    <label htmlFor="name">Your Name</label>
+                    <input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler}
+                           value={enteredName}/>
+                    {nameInputIsInvalid && <p>You must enter your name.</p>}
+                    <label htmlFor='email'>E-mail</label>
+                    <input type='email' id='email'/>
+                    <label htmlFor='topic'>Enter your question topic</label>
+                    <input type='text' id='topic'/>
+                </div>
+                <div>
+                    <button disabled={!formIsValid}>Submit</button>
+                </div>
+            </form>
         </>
 
     )
