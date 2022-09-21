a4b9fbc388def1d089b20773e95e8d2f0b3a8a68
Ulises Orozco <ulisesorozco6@gmail.com>
2022-09-20 20:16:14 -0700

    Created a UI folder which holds the confirmation modal

---
+++ src/components/UI/confirmationModal.js
@@ -0,0 +1,15 @@
+export const confirmationModal = () => {
+    return (
+        <>
+            <header>
+                <h2> Your form has been submitted!</h2>
+            </header>
+            <div>
+                <p>We well send you a confirmation email soon</p>
+            </div>
+            <footer>
+                <button>Okay</button>
+            </footer>
+        </>
+    )
+}
